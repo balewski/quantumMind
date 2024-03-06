b6893cf4e31c4e2c93308e1b9253e83a2a030e5d
Jan Balewski <janstar1122@gmail.com>
2024-03-05 22:32:23 -0800

    ok

--- Qiskit/toy_circ/runAer_parametericCirc.py
+++ Qiskit/toy_circ/runAer_parametericCirc.py
@@ -15,9 +15,11 @@ theta = Parameter('θ')
 phi = Parameter('φ')
 
 # Create a parameterized quantum circuit
-qc = QuantumCircuit(1)
+nq=5
+qc = QuantumCircuit(nq)
 qc.rx(theta, 0)
 qc.ry(phi, 0)
+for iq in range(1,nq): qc.cx(0,iq)
 
 # Print the original parameterized circuit
 print("Original parameterized circuit:")
@@ -27,20 +29,28 @@ print(qc)
 param_values = {theta: 1.57, phi: 3.14}  # Example values: π/2 and π
 
 # Create a new circuit with parameters bound to the specified values
-bound_qc = qc.bind_parameters(param_values)
+bound_qc = qc.assign_parameters(param_values)
 
 # Print the bound circuit
-print("\nCircuit after binding parameters:")
+print("\nCircuit after assigning parameters:")
 print(bound_qc)
 
 # Use Aer's qasm_simulator
 backend = AerSimulator()
+
+if 0:
+    from qiskit.providers.fake_provider  import FakeHanoi
+    fake_backend = FakeHanoi()
+    backend = AerSimulator.from_backend(fake_backend)
 
 # Since we need a measurement to simulate and get counts, add measurement to the bound circuit
 bound_qc.measure_all()
 
 # Transpile the circuit for the simulator
 qcT = transpile(bound_qc, backend)
+qcT=bound_qc
+print(qcT.draw(output='text',idle_wires=False))
+#print('tt',type(qcT._layout))
 #print('ll',qcT._layout.final_index_layout(filter_ancillas=True))
 
 # Execute the circuit
