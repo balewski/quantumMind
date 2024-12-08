92c47175384b2c38104b575286a51c6188db7348
Jan Balewski <balewski@travelr.local>
2024-12-08 11:48:40 +1100

    ok

--- Qiskit/magic_otoc/toy_magic_otoc.py
+++ Qiskit/magic_otoc/toy_magic_otoc.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 __author__ = "Jan Balewski"
 __email__ = "janstar1122@gmail.com"
+'''
+Measure 'magic' for selected unitaries
+Method: OTOC = out-of-time-ordered correlator for Pauli strings
+https://arxiv.org/pdf/2408.01663
+
+*** random unitaries ***
+1 block of 3 qubits U with 3 T-gates
+./toy_magic_otoc.py --Utype rnd 3 1 --U_qubits 3   
+>>> Magic: m=0.82+/-0.04  t=0.86 ; Tcnt: m=2.8+/-0.1  t=3 NPstr=800
+
+2 blocks of 3 qubits U with 2 T-gates, total 4 gates
+./toy_magic_otoc.py --Utype rnd 2 2 --U_qubits 3   
+
+>>> Magic: m=0.52+/-0.03  t=1.15 ; Tcnt: m=1.8+/-0.1  t=4 
+
+*** QFT  ***
+./toy_magic_otoc.py --Utype qft --U_qubits 5 -N2000
+
+*** angle encoding ***
+./toy_magic_otoc.py --Utype ang-enc --U_qubits 9
+
+ thV=np.linspace(-1,1,n)*np.pi
+ for j in range(n): qc.ry(thV[j],j)
+
+*** prob encoding ***
+./toy_magic_otoc.py --Utype prob-enc --U_qubits 9
+
+ thV=np.arccos(np.linspace(-1,1,n))
+ for j in range(n): qc.ry(thV[j],j)
+
+'''
 
 from qiskit import QuantumCircuit,QuantumRegister, ClassicalRegister
 from qiskit_ibm_runtime.options.estimator_options import EstimatorOptions
@@ -19,37 +50,103 @@ def commandline_parser():  # used when runing from command line
     parser = argparse.ArgumentParser()
     parser.add_argument("-v","--verbosity",type=int, help="increase output verbosity", default=1, dest='verb')
 
-    parser.add_argument('-q','--sys_qubits',type=int,default=3, help="qubit count for 'system' U ")
-    parser.add_argument('-t','--num_Tgate',type=int,default=2, help="number of Tgates in system U")
-    parser.add_argument('-n','--num_shots',type=int,default=2000, help="shots")
-    parser.add_argument('-N','--num_Pstrings',type=int,default=5, help="num pairs of Pauli strings")
+    parser.add_argument('-q','--U_qubits',type=int,default=3, help="qubit count for U ")
+    parser.add_argument('-n','--num_shots',type=int,default=3000, help="shots")
+    parser.add_argument('-N','--num_Pstrings',type=int,default=1000, help="num pairs of Pauli strings")
+    parser.add_argument('-U','--Utype',nargs='+',default=['rnd', 3 ], help="type of Unitary with optional params, blankl separated ")
     args = parser.parse_args()
+   
+    if args.Utype[0]=='rnd':  # par: nTgate [nBlock]
+        npar=len(args.Utype)
+        assert npar>1
+        # num Tgates per block
+        args.rndU_nT=int(args.Utype[1])
+        # number of U-blocks 
+        if npar==3: args.rndU_nB=int(args.Utype[2])
+        else: args.rndU_nB=1
+    args.Utype=args.Utype[0]
+    assert args.Utype in ['rnd','qft','ang-enc','prob-enc']
     for arg in vars(args):  print( 'myArg:',arg, getattr(args, arg))
     return args
 
 
 #...!...!....................
-def make_systemU(nq, nT):
+def prepU_angEnc( n): 
+    ''' angle encoding
+    theta= x*pi  for x in [-1,1]
+    '''
+    qc = QuantumCircuit(n)
+    
+    thV=np.linspace(-1,1,n)*np.pi
+    for j in range(n): qc.ry(thV[j],j)
+    return qc,1
+    
+#...!...!....................
+def prepU_probEnc( n): 
+    ''' prob encoding
+    theta= arcos(x)  for x in [-1,1]
+    '''
+    qc = QuantumCircuit(n)
+    
+    thV=np.arccos(np.linspace(-1,1,n))
+    for j in range(n): qc.ry(thV[j],j)
+    return qc,1
+    
+#...!...!....................
+def prepU_qft( n): 
+    qc = QuantumCircuit(n)
+    ''' skip input
+    # For example, prepare a sample state |0011> (binary for 3)
+    qc.x(0)
+    qc.x(1)
+    
+    Apply the QFT on the first n qubits of circuit qc.
+    '''
+    for j in range(n):
+        # Apply H-gate on the j-th qubit
+        qc.h(j)
+        
+        # Apply controlled-phase gates
+        for k in range(j+1, n):
+            # The phase is pi/2^(k-j)
+            qc.cp(np.pi / 2**(k-j), k, j)
+    
+    # Finally, reverse the qubit order
+    for i in range(n//2):
+        qc.swap(i, n - i - 1)
+
+    tTcnt=n*np.log(n)
+    return qc,tTcnt
+
+#...!...!....................
+def prepU_random(nq, nT,nB=1):
     """
     Create a random Clifford circuit,
     next add nT T-gates at the end  at random qubits
+    optional: repeat it nB times
     
     Returns:
     QuantumCircuit: The quantum circuit implementing the unitary.
     """
     assert nq>0
     assert nT<=nq
+    assert nB>=1
 
-    qc=random_clifford(nq).to_circuit()
-    
-    # Randomly choose distinct qubits without repetition
-    chosen_qubits = random.sample(range(nq), nT)
-    
-    # Add a T-gate to each chosen qubit
-    for qb in chosen_qubits:
-        qc.t(qb)
-    
-    return qc
+    qc= QuantumCircuit(nq)
+    for b in range(nB):
+        
+        if b>0 and 0: # OFF insert GHZ
+            qc.h(0)
+            for i in range(1,nq): qc.cx(0,i)
+        qcr=random_clifford(nq).to_circuit().to_gate()
+        qcr.name='clif_%d'%b
+        qc.append(qcr, qargs=range(nq))
+        # Randomly choose distinct qubits without repetition
+        chosen_qubits = random.sample(range(nq), nT)       
+        # Add a T-gate to each chosen qubit
+        for qb in chosen_qubits: qc.t(qb)
+    #1print(qc.decompose())
+    return qc,nT*nB
 
 #...!...!....................
 def random_pauli_string(n):
@@ -69,7 +166,6 @@ def controlled_pauli_circuit(pauli_string):
     total_qubits = n + 1  # Including the control qubit
     qc = QuantumCircuit(total_qubits)
 
-
     for i, p in enumerate(pauli_string):
         target_qubit = i + 1  # Since qubit 0 is the control qubit
         if p == 'I':
@@ -84,6 +180,7 @@ def controlled_pauli_circuit(pauli_string):
             raise ValueError(f"Invalid Pauli operator: {p}")
     return qc
 
+#...!...!....................
 def pauli_circuit(pauli_string):
     """
     Create a QuantumCircuit that:
@@ -108,7 +205,6 @@ def pauli_circuit(pauli_string):
 
     return qc
 
-
 #...!...!....................
 def make_otoc_circ(Usys,verb=1):
     ''' out-of-time-ordered correlator, based on https://arxiv.org/pdf/2408.01663
@@ -118,7 +214,7 @@ def make_otoc_circ(Usys,verb=1):
     '''
     nqs=Usys.num_qubits
     Uinv=Usys.inverse(); Uinv.name='Uinv'
-    if verb>0: print('Usys:'); print(Usys.definition.draw())
+    if verb>0 and nqs<6: print('Usys:'); print(Usys.definition.draw())
   
     P1str=random_pauli_string(nqs)
     P2str=random_pauli_string(nqs)
@@ -126,18 +222,19 @@ def make_otoc_circ(Usys,verb=1):
     if verb>0: print('P1str:',P1str,'  P2str:',P2str)
     P1op = pauli_circuit(P1str).to_gate(label='P1')
     cP2op = controlled_pauli_circuit(P2str).to_gate(label='cP2')
-    if verb>0: 
+    if verb>1: 
         print('P1:'); print(P1op.definition.draw())
         print('cP2:'); print(cP2op.definition.draw())
 
     # Initialize a new quantum circuit with n qubits
     qc3 = QuantumCircuit(2*nqs+1)
-    #qc3.x(0);    return qc3,[P1str,P2str]
+  
     #initial state
     qc3.h(0)
     for i in range(nqs): qc3.h(i+1+nqs)
     for i in range(nqs): qc3.cx(i+1+nqs,i+1)
     qc3.barrier()
+    
     qc3.append(cP2op, qargs=range(nqs+1))
     qsL=[i for i in range(1,nqs+1)]
     qc3.append(Usys, qargs=qsL)
@@ -151,19 +248,31 @@ def make_otoc_circ(Usys,verb=1):
     #print(qc3.decompose())
     #print(qc3)
     return qc3,[P1str,P2str]
-    
+
+#...!...!....................
+def export_CSV_rndU():
+    print('\nU qubits,num T-gate /rndCliff,num rndCliff,num T-gate,true magic,meas magic,err,meas num T-gate,err,num Pauli str,Utype,elaT/min')
+    print('\n%d,%d,%d,%d,%.2f,%.2f,%.2f,%.1f,%.1f,%d,%s,%.1f'% \
+          (nqU,args.rndU_nT,args.rndU_nB,args.rndU_nT*args.rndU_nB,tMagic,mMagic,mMagicErr,mTcnt,mTcntErr,args.num_Pstrings,args.Utype,elaT))
+
 #=================================
 #  M A I N
 #=================================
 if __name__ == "__main__":
     args=commandline_parser()
-    #np.set_printoptions(precision=3)
+    np.set_printoptions(precision=3)
 
-    nqs=args.sys_qubits
-    qcU=make_systemU(nqs,args.num_Tgate)
+    nqU=args.U_qubits
+    if args.Utype=='rnd':
+        qcU,trueTcnt=prepU_random(nqU,args.rndU_nT,args.rndU_nB)  # random U + some Tgates
+    if args.Utype=='qft': qcU,trueTcnt=prepU_qft(nqU)
+    if args.Utype=='ang-enc': qcU,trueTcnt=prepU_angEnc(nqU)
+    if args.Utype=='prob-enc': qcU,trueTcnt=prepU_probEnc(nqU)
+
+    
     # Convert the circuit to a gate
     Uop = qcU.to_gate(label='U')
-    nq=2*nqs+1
+    nq=2*nqU+1
     
     obs = SparsePauliOp("I" * (nq-1)+"Z")   # MSBF
     print('obs:',obs)
@@ -176,23 +285,36 @@ if __name__ == "__main__":
 
     pm = generate_preset_pass_manager(optimization_level=1, backend=backend)
 
-    evL=[0]*args.num_Pstrings
-    T0=time()
+    evA=np.zeros(args.num_Pstrings)
+    T0=time(); lastEV=0
     for j in range(args.num_Pstrings):
         qc,P1P2=make_otoc_circ(Uop,verb=j==0)
-        if j==0: print(qc)
+        if j==0 and  nqU<6 : print(qc)
         qcT = pm.run(qc)
         obsT=obs.apply_layout(qcT.layout)
         job = estimator.run([(qcT,obsT)])    
         result = job.result()
         rdata=result[0].data
-        evL[j]=abs(rdata.evs)
+        evA[j]=abs(rdata.evs)
         if j==0:
             print("Metadata: ",result[0].metadata)
-        if j<15 or j%100==0: print(j,"Expectation value: %7.3f +/- %.3f  P1P2: %s"%(rdata.evs,rdata.stds,P1P2))
+        if j<15 : print(j,"Expectation value: %7.3f +/- %.3f  P1P2: %s"%(rdata.evs,rdata.stds,P1P2))
+        if j%200==100 :
+            avrEV=np.mean(evA[:j]); stdEV=np.std(evA[:j])/np.sqrt(j)
+            delAvr=avrEV-lastEV; nSig=abs(delAvr)/stdEV
+            lastEV=avrEV
+            print('%4d Avr EV: %7.3f+/-%.3f  delEV=%6.3f  nSig=%.1f  elaT=%.1f min'%(j,avrEV,stdEV,delAvr,nSig,(time()-T0)/60.))
+        
     T1=time()
-    avrEV=np.mean(evL)
-    print('avrEV=%.2f'%(avrEV))
-    measIN=-np.log(avrEV)
-    trueIN=args.num_Tgate*np.log(4/3)
-    print('Final I_N meas=%.2f   true=%.2f  numT=%d  N=%d  shots/circ=%d  elaT=%.1f min'%(measIN, trueIN,args.num_Tgate,args.num_Pstrings,args.num_shots,(T1-T0)/60.))
+    avrEV=np.mean(evA); stdEV=np.std(evA)/np.sqrt(evA.shape[0])
+    #print('avrEV=%.2f'%(avrEV))
+    mMagic=-np.log(avrEV)
+    mMagicErr=stdEV/avrEV  # for y=log(x) sig(y)= sig(x)/x
+    tMagic=trueTcnt*np.log(4/3)
+    
+    mTcnt=mMagic/np.log(4/3)
+    mTcntErr=mMagicErr/np.log(4/3)
+    elaT=(T1-T0)/60.
+    print('Magic: m=%.2f+/-%.2f  t=%.2f ; Tgate cnt: m=%.1f+/-%.1f  t=%.1f ; Utype=%s nqU=%d  NPstr=%d  shots/circ=%d  elaT=%.1f min'%(mMagic,mMagicErr, tMagic,mTcnt,mTcntErr,trueTcnt,args.Utype,nqU,args.num_Pstrings,args.num_shots,elaT))
+
+    if args.Utype=='rnd': export_CSV_rndU()
