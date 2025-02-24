6222a0491fd0cbc6d6e904ae3060688a09526dbc
Jan Balewski <janstar1122@gmail.com>
2025-02-24 14:28:00 -0800

    ok

--- IQM/toys/toy_ghz_backRun.py
+++ IQM/toys/toy_ghz_backRun.py
@@ -3,9 +3,9 @@ __author__ = "Jan Balewski"
 __email__ = "janstar1122@gmail.com"
 
 from qiskit import QuantumCircuit,QuantumRegister, ClassicalRegister, transpile
-from qiskit_ibm_runtime import QiskitRuntimeService
-from qiskit_ibm_runtime import SamplerV2 as Sampler
-from qiskit_ibm_runtime.options.sampler_options import SamplerOptions
+#from qiskit_ibm_runtime import QiskitRuntimeService
+#from qiskit_ibm_runtime import SamplerV2 as Sampler
+#from qiskit_ibm_runtime.options.sampler_options import SamplerOptions
 from qiskit_aer import AerSimulator
 from iqm.qiskit_iqm import IQMProvider
 
@@ -29,33 +29,24 @@ print('M: ideal circ gates count:', qc.count_ops())
 
 backend1 = AerSimulator()
 print('job started,  nq=%d  at %s ...'%(qc.num_qubits,backend1.name))
-options = SamplerOptions()
-options.default_shots=10000
+job = backend1.run(qc, shots=1000)
+result = job.result()
+print('Aer:',result.get_counts())
 
-
-qcEL=(qc,)  # quant circ executable list
-sampler = Sampler(mode=backend1, options=options)
-job = sampler.run(qcEL)
-result=job.result()
-counts=result[0].data.meas.get_counts()
-print('counts1:',counts)
 
 print('access IQM backend ...')
 # os.environ["IQM_TOKEN"] is set already
 provider=IQMProvider(url="https://cocos.resonance.meetiqm.com/garnet")
 backend2 = provider.get_backend()
-
 print('got backend:',backend2.name)
 
 qcT = transpile(qc, backend2)
 
 print(qcT.draw('text', idle_wires=False))
 print('M: transpiled GHZ(nq=%d) gates count:'%nq, qcT.count_ops())
-qcETL=(qcT,) 
-sampler = Sampler(mode=backend2, options=options)
-job = sampler.run(qcETL)
-result=job.result()
-counts=result[0].data.meas.get_counts()
-print('counts2:',counts)
 
+job = backend2.run(qcT, shots=1000)
+result = job.result()
+print('IQM:',result.get_counts())
+print(result.get_memory())
 
